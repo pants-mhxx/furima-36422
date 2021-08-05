f474ae6e6182a692f0d93d9e02dc018720d1d737
ShunKondo <stedra0917@gmail.com>
2021-08-05 20:07:03 +0900

    ユーザー管理機能修正1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :first_furigana, :last_furigana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :first_furigana, :last_furigana, :birth_date])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do 
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do 
+      validates :first_furigana
+      validates :last_furigana
+    end
+  end
   
-  validates :password, presence: true, format: { with: /\A[a-z0-9]+\z/i }
+  
+  
 end
