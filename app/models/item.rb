3a06fda06196d0849a17639595dc2d48b18031ea
ShunKondo <stedra0917@gmail.com>
2021-08-06 15:23:33 +0900

    出品ビュー設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
 end
