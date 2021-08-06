4a1ebaf190d02fc03b5a90d1974bfcd07bc5b93e
ShunKondo <stedra0917@gmail.com>
2021-08-06 19:30:59 +0900

    createコントローラー作成(途中)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
     
   end
@@ -8,10 +9,19 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      binding.pry
+      redirect_to root_path
+    else
+      redirect_to new_item_path, alert: @item.errors.full_messages
+    end
   end
 
   private
 
-  
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :item_state_id, :shipping_fee_id, :shipping_from_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
