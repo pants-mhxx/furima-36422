0de55b3d923da095d80a93f535a3752a9b26014d
ShunKondo <stedra0917@gmail.com>
2021-08-09 20:31:21 +0900

    move_to_index実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -39,5 +40,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :item_state_id, :shipping_fee_id, :shipping_from_id,
                                  :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    if item.user_id != current_user.id
+      redirect_to action: :index
+    end
   end
 end
