4e1cbb115683d03ec01eb2f8b2eb218548bd597a
ShunKondo <stedra0917@gmail.com>
2021-08-11 11:50:51 +0900

    Formオブジェクト設定

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,4 +1,19 @@
-class PuchaseAddress
+class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :zip, :city, :address_line1, :address_line2, :telephone, :building, :purchase
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :address_line1
+    validates :address_line2
+    validates :telephone
+  end
+  validates :city, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    purchase = Purchase.create(item: item_id, user_id: user_id)
+    Address.create(zip: zip, city: city, address_line1: address_line1, address_line2: address_line2, building: building, telephone: telephone)
+  end
 end
