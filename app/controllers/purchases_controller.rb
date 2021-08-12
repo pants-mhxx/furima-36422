3cef1a3326561c10f3bcdc49d0da60307e1a1123
ShunKondo <stedra0917@gmail.com>
2021-08-12 16:29:50 +0900

    エラーメッセージ実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -16,7 +16,6 @@ class PurchasesController < ApplicationController
       @purchase_address.save
       redirect_to root_path
     else
-      binding.pry
       @item = Item.find(params[:item_id])
       render :index
     end
